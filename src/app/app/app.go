5866c807edb263eb95f9d9f7ec52d6b7f814a6bf
Miguel Eduardo Osorio Montilla <mosoriom@psl.com.co>
2017-02-20 16:48:50 -0500

    restructured app's packages

--- src/app/app/app.go
+++ src/app/app/app.go
@@ -1,4 +1,4 @@
-package models
+package app
 
 import (
 	"github.com/labstack/echo"
--- src/app/app/type.go
+++ src/app/app/type.go
@@ -1,4 +1,4 @@
-package models
+package app
 
 // Confirmation generic request confirmation.
 type Confirmation struct {
